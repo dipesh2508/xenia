17e470f9b756efff8d7efbf821517c01a2b0670a
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2025-02-15 18:44:49 +0530

    fix:import alias

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@repo/ui/components/ui/button";
 import { Card } from "@repo/ui/components/ui/card";
 import React from 'react'
-
+import Abc from "@/components/Abc";
 const page = () => {
   return (
     <div className="h-screen flex flex-col items-center justify-center space-y-4">
@@ -17,6 +17,8 @@ const page = () => {
       <Card className="w-64 shadow-2xl">
         Card content
       </Card>
+
+      <Abc />
     </div>
   )
 }
