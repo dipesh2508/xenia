fca75c131e28b9089e625817860364659826dddf
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2025-03-19 20:57:42 +0530

    config:modified next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,12 +7,31 @@ const nextConfig = {
         {
           source: '/api/:path*',
           destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/:path*`,
+          has: [
+            {
+              type: 'cookie',
+              key: 'token',
+            },
+          ],
         },
         {
           source: '/socket.io/:path*',
           destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/socket.io/:path*`,
         }
       ];
+    },
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date' },
+          ],
+        }
+      ]
     },
     // Enable WebSocket connections in development
     webpack: (config) => {
